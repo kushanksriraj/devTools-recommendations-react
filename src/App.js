48bf68100164f9540d46f79113ccead27673fc8c
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-07 00:10:01 +0530

    created basic layout and added style

--- src/App.js
+++ src/App.js
@@ -4,8 +4,34 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="header"> devTools
+      <p>some recommended dev tools</p>
+      </div>
+    
+    <main>
+    
+    <div className="leftPanel">
+      {/* <p><strong>Click to see recommendations</strong></p> */}
+      <button className="leftPanelButton">code</button>
+      <button className="leftPanelButton">docs</button>
+      <button className="leftPanelButton">webdev</button>
+      <button className="leftPanelButton">devops</button>
+    </div>
+
+    <div className="rightPanel">
+      {/* This is right leftPanel */}
+     
+     <ul>
+       <li>recommendations<span>5/5</span></li>
+       <li>recommendations<span>4/5</span></li>
+       <li>recommendations<span>4.5/5</span></li>
+       <li>recommendations<span>3/5</span></li>
+     </ul>
+
+    </div>
+
+   </main>
+   
     </div>
   );
 }
